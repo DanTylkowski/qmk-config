4b6f529699a2c850a1a2bdc0bf628991ac8d2700
Dan Tylkowski <dan.tylkowski@selectablemedia.com>
2020-12-17 20:56:06 -0500

    various keymap adjustments

--- keyboards/spaceman/pancake/keymaps/dan/keymap.c
+++ keyboards/spaceman/pancake/keymaps/dan/keymap.c
@@ -45,7 +45,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   * ,-----------------------------------------------------------------------------------------------------------------------------------------------.
   * | TRNS (P0) |   ! (P1)  |   @ (P1)  |   # (P1)  |   $ (P1)  |   % (P1)  |   ^ (P1)  |   & (P1)  |   * (P1)  |   ( (P1)  |   ) (P1)  | TRNS (P0) |
   * |-----------+-----------+-----------+-----------+-----------+-----------+-----------+-----------+-----------+-----------+-----------+-----------|
-  * | TRNS (P0) |  _  (P1)  |  +  (P1)  |  {  (P1)  |  }  (P1)  |  ~  (P1)  |  |  (P1)  |  NO  (P1) |  NO (P1)  |  NO (P1)  |  NO (P2)  | NO  (P2)  |
+  * | TRNS (P0) |  _  (P1)  |  +  (P1)  |  {  (P1)  |  }  (P1)  |  |  (P1)  |  ~  (P1)  |  NO  (P1) |  NO (P1)  |  NO (P1)  |  NO (P2)  | NO  (P2)  |
   * |-----------+-----------+-----------+-----------+-----------+-----------|-----------+-----------+-----------+-----------+-----------+-----------|
   * | TRNS (P4) |  NO (P1)  |  NO (P1)  |  NO (P1)  |  NO (P1)  |  NO (P1)  |  NO (P1)  |  NO (P1)  |  NO (P2)  |  NO (P2)  | Vol+ (P3) | TRNS (P0) |
   * |-----------+-----------+-----------+-----------+-----------+-----------+-----------+-----------+-----------+-----------+-----------+-----------|
@@ -54,7 +54,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   */
   [_LOWER] = LAYOUT_planck_mit(
       KC_TRNS, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_TRNS,
-      KC_TRNS, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_TILD, KC_PIPE, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
+      KC_TRNS, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE, KC_TILD, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
       KC_TRNS, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_VOLU, KC_TRNS,
       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,      KC_TRNS,     KC_TRNS, KC_NO,   KC_BRID, KC_VOLD, KC_BRIU
   ),
@@ -62,7 +62,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   * ,-----------------------------------------------------------------------------------------------------------------------------------------------.
   * | TRNS (P0) |   1 (P1)  |   2 (P1)  |   3 (P1)  |   4 (P1)  |   5 (P1)  |   6 (P1)  |   7 (P1)  |   8 (P1)  |   9 (P1)  |   0 (P1)  | TRNS (P0) |
   * |-----------+-----------+-----------+-----------+-----------+-----------+-----------+-----------+-----------+-----------+-----------+-----------|
-  * | TRNS (P0) |  -  (P1)  |  =  (P1)  |  [  (P1)  |  ]  (P1)  |  `  (P1)  |  \  (P1)  |  NO (P1)  |  NO (P1)  |  NO (P1)  |  NO (P2)  |  NO (P2)  |
+  * | TRNS (P0) |  -  (P1)  |  =  (P1)  |  [  (P1)  |  ]  (P1)  |  \  (P1)  |  `  (P1)  |  NO (P1)  |  NO (P1)  |  NO (P1)  |  NO (P2)  |  NO (P2)  |
   * |-----------+-----------+-----------+-----------+-----------+-----------|-----------+-----------+-----------+-----------+-----------+-----------|
   * | TRNS (P4) |  NO (P1)  |  NO (P1)  |  NO (P1)  |  NO (P1)  |  NO (P1)  |  NO (P1)  |  NO (P1)  |  NO (P2)  |  NO (P2)  | Play (P3) | TRNS (P0) |
   * |-----------+-----------+-----------+-----------+-----------+-----------+-----------+-----------+-----------+-----------+-----------+-----------|
@@ -71,7 +71,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   */
   [_RAISE] = LAYOUT_planck_mit(
       KC_TRNS,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_TRNS,
-      KC_TRNS,  KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_GRV,  KC_BSLS, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
+      KC_TRNS,  KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS, KC_GRV,  KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
       KC_TRNS,  KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_MPLY, KC_TRNS,
       KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,      KC_TRNS,     KC_TRNS, KC_NO,   KC_MPRV, KC_MSTP, KC_MNXT
   ),

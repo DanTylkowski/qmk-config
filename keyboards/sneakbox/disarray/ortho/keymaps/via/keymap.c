81aa857f9695fd227de6300495c82c6cc73b4296
Dan Tylkowski <dan.tylkowski@selectablemedia.com>
2021-11-13 13:28:16 -0500

    some sorta changes

--- keyboards/sneakbox/disarray/ortho/keymaps/via/keymap.c
+++ keyboards/sneakbox/disarray/ortho/keymaps/via/keymap.c
@@ -76,7 +76,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
     [_L3] = LAYOUT(
         KC_TRNS,
-        KC_TRNS,  KC_TRNS,    KC_TRNS,   KC_TRNS,  KC_TRNS,  KC_NLCK,  KC_TRNS,     KC_TRNS,      KC_TRNS,      KC_BSPC,   KC_PMNS,   KC_PPLS,   KC_LPRN,   KC_RPRN,   KC_TRNS,   KC_TRNS,
+        KC_TRNS,  KC_TRNS,    KC_TRNS,   KC_TRNS,  KC_TRNS,  KC_NLCK,  KC_P7,       KC_P8,        KC_P9,        KC_BSPC,   KC_PMNS,   KC_PPLS,   KC_LPRN,   KC_RPRN,   KC_TRNS,   KC_TRNS,
         KC_TRNS,  KC_TRNS,    KC_TRNS,   KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,     KC_TRNS,      KC_TRNS,      KC_PPLS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_PSLS,   KC_TRNS,
         KC_TRNS,  KC_TRNS,    KC_TRNS,   KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,     KC_TRNS,      KC_TRNS,      KC_PPLS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,
         KC_TRNS,  KC_TRNS,    KC_PAST,   KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,     KC_TRNS,      KC_TRNS,      KC_PPLS,   KC_TRNS,   KC_LPRN,   KC_RPRN,   KC_PSLS,   KC_TRNS,   KC_TRNS,

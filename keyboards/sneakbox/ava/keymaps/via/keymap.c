41a0e62fabbca821ba0a4de9c680b23a5c93a05a
Dan Tylkowski <dan.tylkowski@selectablemedia.com>
2022-02-02 20:29:54 -0500

    ava

--- keyboards/sneakbox/ava/keymaps/via/keymap.c
+++ keyboards/sneakbox/ava/keymaps/via/keymap.c
@@ -40,16 +40,123 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         RESET,     KC_TRNS,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,    RGB_SAD,   RGB_SAI,    KC_TRNS,    RGB_HUI,
                    KC_TRNS,   KC_TRNS,                   KC_TRNS, KC_TRNS,                       KC_TRNS, KC_TRNS,                                 KC_TRNS,    KC_TRNS,    KC_TRNS),
     [_L3] = LAYOUT_ava_split_bs(
-        KC_TRNS,   KC_TRNS,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,    KC_TRNS,               KC_TRNS,    KC_TRNS, 
+        KC_TRNS,   KC_TRNS,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,    KC_TRNS,               KC_TRNS,    KC_TRNS,
         KC_TRNS,   KC_TRNS,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,    KC_TRNS,   KC_TRNS,    KC_TRNS,    KC_TRNS,
         KC_TRNS,   KC_TRNS,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,    KC_TRNS,   KC_TRNS,                KC_TRNS,
         KC_TRNS,   KC_TRNS,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,    KC_TRNS,   KC_TRNS,    KC_TRNS,    KC_TRNS,
                    KC_TRNS,   KC_TRNS,                   KC_TRNS, KC_TRNS,                       KC_TRNS, KC_TRNS,                                 KC_TRNS,    KC_TRNS,    KC_TRNS),
     [_L4] = LAYOUT_ava_split_bs(
-        KC_TRNS,   KC_TRNS,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,    KC_TRNS,               KC_TRNS,    KC_TRNS, 
+        KC_TRNS,   KC_TRNS,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,    KC_TRNS,               KC_TRNS,    KC_TRNS,
         KC_TRNS,   KC_TRNS,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,    KC_TRNS,   KC_TRNS,    KC_TRNS,    KC_TRNS,
         KC_TRNS,   KC_TRNS,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,    KC_TRNS,   KC_TRNS,                KC_TRNS,
         KC_TRNS,   KC_TRNS,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,    KC_TRNS,   KC_TRNS,    KC_TRNS,    KC_TRNS,
                    KC_TRNS,   KC_TRNS,                   KC_TRNS, KC_TRNS,                       KC_TRNS, KC_TRNS,                                 KC_TRNS,    KC_TRNS,    KC_TRNS),
 };
 
+static const uint16_t ENCODER_TOGGLE_KEY = KC_MHEN;
+static const uint16_t ALFRED_MOD_KEY = LT(0, KC_NO);
+// static const uint16_t UC_SHRG = LT(4, KC_NO);
+
+enum Mode {
+    SCROLL,
+    WINDOW,
+    APPS,
+
+    _NUM_MODES,
+};
+
+static enum Mode cur_mode = SCROLL;
+static enum Mode throttle = SCROLL;
+
+
+bool encoder_update_user(uint8_t index, bool clockwise) {
+    switch (cur_mode) {
+        case SCROLL:
+            tap_code(clockwise ? KC_MS_WH_DOWN : KC_MS_WH_UP);
+            break;
+
+        case WINDOW:
+            if(throttle == 0 ) {
+                tap_code16(clockwise ? LGUI(KC_GRV) : LSG(KC_GRV));
+            }
+            throttle = (throttle + 1) % 8;
+            break;
+        case APPS:
+            if(throttle == 0 ) {
+                tap_code16(clockwise ? LSG(KC_TAB) : LGUI(KC_TAB));
+            }
+            throttle = (throttle + 1) % 8;
+            break;
+
+        default:
+            break;
+    }
+    return true;
+}
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    switch( keycode ) {
+        case ALFRED_MOD_KEY:
+            if (record->tap.count && record->event.pressed) {
+                tap_code16(C(KC_SPC));
+            } else if (record->event.pressed) {
+                register_code(KC_LCTL);
+                register_code(KC_LALT);
+            } else {
+                unregister_code(KC_LCTL);
+                unregister_code(KC_LALT);
+            }
+        return false;
+        case ENCODER_TOGGLE_KEY:
+            if (record->event.pressed) {
+                cur_mode = (cur_mode + 1) % _NUM_MODES;
+            }
+        return false;
+        // case UC_SHRG:  // ¯\_(ツ)_/¯
+        //     if (record->event.pressed) {
+        //         send_unicode_hex_string("175 92 95 40 12484 41 95 47 175");
+        //     }
+        // return false;
+
+    }
+    return true;
+}
+//send_unicode_hex_string("175 92 95 40 12484 41 95 47 175");
+
+static uint8_t top = 1;
+static uint8_t left = 1;
+static uint8_t right = 1;
+
+void keyboard_pre_init_user(void) {
+  setPinOutput(D4);
+  setPinOutput(D6);
+  setPinOutput(D7);
+}
+
+layer_state_t layer_state_set_user(layer_state_t state) {
+  switch (get_highest_layer(state)) {
+  case _FN:
+    top = 0;
+    break;
+  case _L3:
+    left = 0;
+    break;
+  case _L4:
+    right = 0;
+    break;
+  default:
+    top = 1;
+    left = 1;
+    right = 1;
+    break;
+  }
+
+  return state;
+}
+
+bool led_update_user(led_t usb_led) {
+  writePin(D4, !right);
+  writePin(D6, !left);
+  writePin(D7, !top);
+  return false;
+}
